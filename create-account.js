1e0f5dab2f698cbaf9c786e389076eac3ea1e24d
Nathan Soucier <soucienp@gmail.com>
2024-07-08 16:07:20 -0400

    completed bank account challenge

--- create-account.js
+++ create-account.js
@@ -1,5 +1,34 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount=0) {
+    return {
+        checkBalance: (pinIn) => {
+            return pinIn === pin ? `$${amount}` : 'Invalid PIN.' 
+        },
+        deposit: (pinIn, deposit) => {
+            if (pinIn === pin) {
+                amount += deposit;
+                return `Succesfully deposited $${deposit}. Current balance: $${amount}.`
+            } else {
+                return 'Invalid PIN.'
+            }
+        },
+        withdraw: (pinIn, withdrawal) => {
+            if (pinIn === pin) {
+                if (withdrawal > amount) return 'Withdrawal amount exceeds account balance. Transaction cancelled.'
+                amount -= withdrawal;
+                return `Succesfully withdrew $${withdrawal}. Current balance: $${amount}.`
+            } else {
+                return 'Invalid PIN.'
+            }
+        },
+        changePin: (pinIn, newPin) => {
+            if (pinIn === pin) {
+                pin = newPin;
+                return 'PIN successfully changed!'
+            } else {
+                return 'Invalid PIN.'
+            }
+        }
+    }
 }
 
 module.exports = { createAccount };
