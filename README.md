02e69122fcdd9499deedd9bb847b236e956fc2d5
Nathan Soucier <soucienp@gmail.com>
2024-07-08 15:49:43 -0400

    finished first question

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Tricky-Parts-JS
+
+
+To test, run:
+  npm i jest
+  jest filename.test.js
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,21 @@
 function guessingGame() {
-
+  let target = Math.floor(Math.random() * 100);
+  let count = 1;
+  let winner = false;
+  return function (guess) {
+    if (winner) {
+      return `The game is over, you already won!`;
+    } else if (guess === target) {
+      winner = true;
+      return `You win! You found ${target} in ${count} guesses.`;
+    } else if (guess < target) {
+        count++;
+        return `${guess} is too low!`
+    } else {
+        count++;
+        return `${guess} is too high!`
+    }
+  };
 }
 
 module.exports = { guessingGame };
